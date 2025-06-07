ed08c4aa0c2bccf874ffe31c9f739bb2d0cee330
pdarcuk <pdarcuk@gmail.com>
2025-06-07 22:01:28 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,13 +1,9 @@
 window.APP_SETTINGS = {
-
-  speechKey:    "DOI1uG6jZk8Q4QmsFnuqNqSOImaBInOEEcnErRHnQWOoBVyVVPhNJQQJ99BFACfhMk5XJ3w3AAAYACOGiQBg",
-  speechRegion: "swedencentral",
-
-  openaiKey:   "3790GAPIdGQ70caYEO0XwgkBAPuBWT6ZnzCv79akmGhid6kSAjg1JQQJ99BFACfhMk5XJ3w3AAABACOGbYjo",
-  openaiBase:  "https://demo-openai-pd.openai.azure.com",
-  deployment:  "gpt-4o-mini",
-  apiVersion:  "2025-01-01-preview",
-
-  blobSasUrl: "https://storagepdtest01.blob.core.windows.net/audio?sp=w&st=2025-06-07T19:51:46Z&se=2025-07-11T03:51:46Z&spr=https&sv=2024-11-04&sr=c&sig=CFrNaagyC3WZZFWrr15yXZrAAsGF4XpI5gMS%2FPUwL1o%3D"
-
+  speechKey:    import.meta.env.speech_Key,
+  speechRegion: import.meta.env.speech_Region,
+  openaiKey:    import.meta.env.openai_Key,
+  openaiBase:   import.meta.env.openai_Base,
+  deployment:   import.meta.env.openai_deployment,
+  apiVersion:   import.meta.env.api_Version,
+  blobSasUrl:   import.meta.env.blob_Sas_Url
 };

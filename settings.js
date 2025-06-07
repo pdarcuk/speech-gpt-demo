f2b19bd08cf08f05170333af19adea514f7c876e
pdarcuk <pdarcuk@gmail.com>
2025-06-07 21:38:45 +0100

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,13 @@
+window.APP_SETTINGS = {
+
+  speechKey:    "DOI1uG6jZk8Q4QmsFnuqNqSOImaBInOEEcnErRHnQWOoBVyVVPhNJQQJ99BFACfhMk5XJ3w3AAAYACOGiQBg",
+  speechRegion: "swedencentral",
+
+  openaiKey:   "3790GAPIdGQ70caYEO0XwgkBAPuBWT6ZnzCv79akmGhid6kSAjg1JQQJ99BFACfhMk5XJ3w3AAABACOGbYjo",
+  openaiBase:  "https://demo-openai-pd.openai.azure.com",
+  deployment:  "gpt-4o-mini",
+  apiVersion:  "2025-01-01-preview",
+
+  blobSasUrl: "https://storagepdtest01.blob.core.windows.net/audio?sp=w&st=2025-06-07T19:51:46Z&se=2025-07-11T03:51:46Z&spr=https&sv=2024-11-04&sr=c&sig=CFrNaagyC3WZZFWrr15yXZrAAsGF4XpI5gMS%2FPUwL1o%3D"
+
+};

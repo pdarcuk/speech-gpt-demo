2065832ccc6c5b1d73c52b76019dc3f2afbedfef
pdarcuk <pdarcuk@gmail.com>
2025-06-07 22:20:46 +0100

    Update getConfig.js

--- api/getConfig.js
+++ api/getConfig.js
@@ -1,1 +1,15 @@
-//hold
+export async function get(_, context) {
+  return {
+    status: 200,
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      speechKey:    process.env.speech_Key,
+      speechRegion: process.env.speech_Region,
+      openaiKey:    process.env.openai_Key,
+      openaiBase:   process.env.openai_Base,
+      deployment:   process.env.openai_deployment,
+      apiVersion:   process.env.api_Version,
+      blobSasUrl:   process.env.blob_Sas_Url
+    })
+  };
+}
